e618c06c23a336ef581102171deb635aeb5cfaf1
ijoosong <ijoosong@gmail.com>
2015-04-14 21:58:56 -0400

    finished word-count

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,3 +1,13 @@
-def word_count(word):
+def word_count(sentence):
     #counting words
-    
+    words = sentence.split()
+    uniq_words = set(words)
+    word_dict = {}
+    for word in uniq_words:
+        word_dict.update({word: 0})
+    for word in words:
+        if word in word_dict:
+            word_dict[word]=word_dict[word] + 1
+
+    return word_dict
+
